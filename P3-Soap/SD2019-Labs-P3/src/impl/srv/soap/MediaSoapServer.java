1dbeed0a5dcb88890350fafd5cfc6c037d5ea685
smd <smd@fct.unl.pt>
2019-03-20 19:09:13 +0000

    MediaStorage in SOAP. First draft...

---
+++ P3-Soap/SD2019-Labs-P3/src/impl/srv/soap/MediaSoapServer.java
@@ -0,0 +1,43 @@
+package impl.srv.soap;
+
+import java.net.InetSocketAddress;
+import java.util.logging.Logger;
+
+import javax.xml.ws.Endpoint;
+
+import com.sun.net.httpserver.HttpServer;
+
+import utils.IP;
+
+@SuppressWarnings("restriction")
+public class MediaSoapServer {
+	private static Logger Log = Logger.getLogger(MediaSoapServer.class.getName());
+
+	static {
+		System.setProperty("java.net.preferIPv4Stack", "true");
+		System.setProperty("java.util.logging.SimpleFormatter.format", "%4$s: %5$s\n");
+	}
+	
+	public static final int PORT = 7777;
+	public static final String SERVICE = "Microgram-MediaStorage";
+	public static String SOAP_BASE_PATH = "/soap";
+	
+	public static void main(String[] args) throws Exception {
+		
+        // Create an HTTP server, accepting requests at PORT (from all local interfaces)
+		HttpServer server = HttpServer.create(new InetSocketAddress("0.0.0.0", PORT), 0);
+
+		// Create the SOAP Endpoint
+		Endpoint soapEndpoint = Endpoint.create(new MediaWebService());
+		
+        // Publish the SOAP webservice, under the "http://<ip>:<port>/soap" 
+        soapEndpoint.publish( server.createContext(SOAP_BASE_PATH));
+
+       
+        // Start Serving Requests: both SOAP Requests
+        server.start();
+        
+		String ip = IP.hostAddress();
+		Log.info(String.format("%s Soap Server ready @ %s\n", SERVICE, ip + ":" + PORT));
+	}
+}
---
+++ P3-Soap/SD2019-Labs-P3/src/impl/srv/soap/MediaWebService.java
@@ -0,0 +1,46 @@
+package impl.srv.soap;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import javax.jws.WebService;
+
+import microgram.api.soap.MicrogramException;
+import microgram.api.soap.SoapMedia;
+import utils.Hash;
+
+@WebService(serviceName=SoapMedia.NAME, targetNamespace=SoapMedia.NAMESPACE, endpointInterface=SoapMedia.INTERFACE)
+public class MediaWebService implements SoapMedia {
+
+	private static final String MEDIA_EXTENSION = ".jpg";
+	private static final String ROOT_DIR = "/tmp/microgram/";
+	
+	MediaWebService() {
+		new File( ROOT_DIR ).mkdirs();
+	}
+	
+	@Override
+	public String upload(byte[] bytes) throws MicrogramException {
+		try {
+			String id = Hash.of(bytes);
+			File filename = new File(ROOT_DIR + id + MEDIA_EXTENSION);
+			
+			if( filename.exists() )
+				throw new MicrogramException("Conflict...");
+			
+			Files.write(filename.toPath(), bytes);
+			
+			return id;
+			
+		} catch( IOException x  ) { 
+			x.printStackTrace();
+			throw new MicrogramException("Internal Error..." + x.getMessage());
+		}
+	}
+
+	@Override
+	public byte[] download(String id) throws MicrogramException {		
+		throw new MicrogramException("Not implemented...");
+	}
+}
